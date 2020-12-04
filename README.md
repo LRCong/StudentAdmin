24562e8af656ad057b09a87985c6121a3ec4f511
LRCong <1323794482@qq.com>
2020-12-04 16:42:48 +0800

    初始布局

--- README.md
+++
@@ -1,2 +0,0 @@
-# StudentDataAdmin
-一个学生信息管理系统，由React构建
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import AdminLayout from './pages/Layout';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AdminLayout />
   </React.StrictMode>,
   document.getElementById('root')
 );
